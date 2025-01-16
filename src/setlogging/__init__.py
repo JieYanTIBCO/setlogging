07f49f45ed7813fd04e28e8acb2d07b851653488
JieYanTIBCO <kiki3890528@gmail.com>
2025-01-16 07:05:20 -0500

    change version

--- src/setlogging/__init__.py
+++ src/setlogging/__init__.py
@@ -1,5 +1,5 @@
 # src/setlogging/__init__.py
 from .logger import setup_logging, get_logger, TimezoneFormatter
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __all__ = ["setup_logging", "get_logger", "TimezoneFormatter"]
