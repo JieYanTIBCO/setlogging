58a95eab22a76a1c10e7d3b1155b710369fc059a
JieYanTIBCO <kiki3890528@gmail.com>
2025-01-20 23:12:34 -0500

    ci: upgrade version to 0.3.0

--- src/setlogging/__init__.py
+++ src/setlogging/__init__.py
@@ -1,5 +1,5 @@
 # src/setlogging/__init__.py
 from .logger import setup_logging, get_logger, CustomFormatter
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 __all__ = ["setup_logging", "get_logger", "CustomFormatter"]
